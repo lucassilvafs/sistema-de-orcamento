94e1a9cff5cd45e4b32ca8b4db380c210c5b5c40
Lucas Silva <lucasfic2017@gmail.com>
2024-08-14 03:35:28 -0300

    trocando o titulo

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import Products from "./routes/Products";
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import GlobalStyle from "../../styles/global";
 import Header from "../../components/Header";
-import Resume from "../../components/Resume";
 import Form from "../../components/Form";
 
 const Home = () => {
